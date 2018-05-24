dda12ef2c06d74b9181748165e2f5702602030e0
asura135 <34566596+asura135@users.noreply.github.com>
2018-05-24 11:04:15 +0200

    css

--- view/menu_index.php
+++ view/menu_index.php
@@ -1,17 +1,23 @@
-<div >
+<div class="fgroup">
     <form method="post" action="">
-        <div class="fgroup">
         <div class="row">
-        <label> Fixe Einnahmen </label>
-        <input type="text" name="fixE">
+            <div class="col-md-6 labell">
+                <label> Fixe Einnahmen </label>
+            </div>
+            <div class="col field">
+                <input type="text" name="fixE">
+            </div>
         </div>
-        <div>
-        <label> Fixe Ausgaben </label>
-        <input type="text" name="fixA">
-        </div>
+        <div class="row">
+            <div class="col-md-6 labell">
+                <label> Fixe Ausgaben </label>
+            </div>
+            <div class="col field">
+                <input type="text" name="fixA">
+            </div>
         </div>
         <div class="ok">
-        <input type="submit" name="ok" value="OK">
+            <input type="submit" name="ok" value="OK" class=" btn btn-primary">
         </div>
     </form>
 </div>
